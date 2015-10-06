bea6758d6d1420294bd6661ac79768c956fee9d0
Arthur Aukhatov <aaukhatov@gmail.com>
2015-10-06 20:35:57 +0500

    Общий класс для объектов

---
+++ engine/include/object.h
@@ -0,0 +1,13 @@
+#include <engine.h>
+class Object {
+    private:
+        double x,y,angle;
+    public:
+        double getX();
+        void setX(double x);
+        double getY();
+        void setY(double y);
+        double getAngle();
+        void setAngle(double angle);
+        virtual void think(Engine *e);
+};
---
+++ engine/object.cxx
@@ -0,0 +1,20 @@
+#include <object.h>
+
+void Object::setX(double x) {
+	this.x = x;
+}
+double Object::getX() {
+	return this.x;
+}
+void Object::setY(double y) {
+	this.y = y;
+}
+double Object::getY() {
+	return this.y;
+}
+void setAngle(double angle) {
+	this.angle = angle;
+}
+double getAngle() {
+	return this.angle;
+}
